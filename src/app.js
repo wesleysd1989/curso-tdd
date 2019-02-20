cd6f4311bc07336969b97cb698e755d79fc3e4de
m3n0r <wesleysd1989@gmail.com>
2019-02-20 01:50:23 -0300

    add consign and test users

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false })
+    .include('./config/middlewares.js')
+    .into(app);
 
 app.get('/', (req, res) => {
     res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+    const users =[
+        {name: 'wesley dias', mail: 'wesley@mail.com'},
+    ];
+    res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body);
 });
 
 module.exports = app;
