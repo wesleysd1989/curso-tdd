d7616d2f75cff5b27d6dbcc3b0e6c133be8c1333
m3n0r <wesleysd1989@gmail.com>
2019-02-22 01:52:04 -0300

    test Não deve inserir usuário sem nome

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -21,3 +21,12 @@ test('Deve inserir usuário com sucesso', () => {
     })
 });
 */
+
+test('Não deve inserir usuário sem nome', () => {
+    return request(app).post('/users')
+    .send({ email: 'wesley@mail.com', password: '123456'})
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Nome é um atributo obrigatório.');
+    });
+});
