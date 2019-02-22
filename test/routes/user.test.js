e3a616185ab9023a4d5d2b4cf12876282a26a7fe
m3n0r <wesleysd1989@gmail.com>
2019-02-22 02:07:09 -0300

    test Não deve inserir usuário sem senha

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -37,3 +37,14 @@ test('Não deve inserir usuário sem email', async () => {
     expect(result.status).toBe(400);
     expect(result.body.error).toBe('Email é um atributo obrigatório.');
 });
+
+test('Não deve inserir usuário sem senha', (done) => {
+    request(app).post('/users')
+        .send({ name: 'wesley dias', email: 'wesley@mail.com'})
+        .then((res) => {
+            expect(res.status).toBe(400);
+            expect(res.body.error).toBe('Senha é um atributo obrigatório.');
+            done();
+        })
+        .catch(err => done.fail(err));
+});
