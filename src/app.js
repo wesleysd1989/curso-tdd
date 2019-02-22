7c6de391501f76929f01e48fb74ef72b7a0a9166
m3n0r <wesleysd1989@gmail.com>
2019-02-22 01:34:03 -0300

    logs db

--- src/app.js
+++ src/app.js
@@ -18,6 +18,11 @@ app.get('/', (req, res) => {
     res.status(200).send();
 });
 
-
+/*app.db.on('query', (query) => {
+    console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(','): ''})
+})
+    .on('query-response', response => console.log(response))
+    .on('error' , error => console.log(error));
+*/
 
 module.exports = app;
