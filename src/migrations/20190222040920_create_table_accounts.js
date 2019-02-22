e35062e66be4610c72bda1725d38efffbd87e3d5
m3n0r <wesleysd1989@gmail.com>
2019-02-22 04:16:47 -0300

    create schema accounts

---
+++ src/migrations/20190222040920_create_table_accounts.js
@@ -0,0 +1,16 @@
+
+exports.up = (knex) => {
+    return knex.schema.createTable('accounts', (t) => {
+        t.increments('id').primary();
+        t.string('name').notNull();
+        t.integer('user_id')
+        .references('id')
+        .inTable('users')
+        .notNull();
+    });
+  };
+  
+  exports.down = (knex) => {
+    return knex.schema.dropTable('accounts');
+  };
+  
