cb00e641791ca0bea11bbc2a5eeb243edd90acea
m3n0r <wesleysd1989@gmail.com>
2019-02-26 05:51:37 -0300

    refactory error functions

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,16 @@
 module.exports = (app) => {
     const findAll = (req, res) => {
         app.services.user.findAll()
-        .then(result => res.status(200).json(result));
+            .then(result => res.status(200).json(result));
     };
-    
+
     const create = async (req, res) => {
-        const result = await app.services.user.save(req.body);
-        if(result.error) return res.status(400).json(result)
-         return res.status(201).json(result[0]);
+        try {
+            const result = await app.services.user.save(req.body);
+            return res.status(201).json(result[0]);
+        } catch (err) {
+            return res.status(400).json({ error: err.message })
+        }
     };
 
     return { findAll, create }
--- src/services/user.js
+++ src/services/user.js
@@ -1,15 +1,17 @@
+const ValidationError = require('../errors/ValidationError');
+
 module.exports = (app) => {
     const findAll = (filter = {}) => {
         return app.db('users').where(filter).select();
     };
 
     const save = async (user) => {
-        if(!user.name) return { error: 'Nome é um atributo obrigatório.' };
-        if(!user.email) return { error: 'Email é um atributo obrigatório.' };
-        if(!user.password) return { error: 'Senha é um atributo obrigatório.' };
+        if(!user.name) throw new ValidationError('Nome é um atributo obrigatório.');
+        if(!user.email) throw new ValidationError('Email é um atributo obrigatório.');
+        if(!user.password) throw new ValidationError('Senha é um atributo obrigatório.');
 
         const userDb = await findAll({email: user.email});
-        if(userDb && userDb.length > 0) return {error: 'Já existe um usuário com esse email.'};
+        if(userDb && userDb.length > 0) throw new ValidationError('Já existe um usuário com esse email.');
         
         return app.db('users').insert(user, '*');
     };
