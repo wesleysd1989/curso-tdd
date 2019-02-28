632ff7d1019b709cb2f6b2f0286525b9dbf8a320
m3n0r <wesleysd1989@gmail.com>
2019-02-28 02:58:58 -0300

    protect routes

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 test('Deve receber token ao logar', () => {
-    const email = `${Date.now()}@mail.com`
+    const email = `${Date.now()}@mail.com`;
     return app.services.user.save({ name: 'maria veridiane', email , password: '123456' })
     .then(() => request(app).post('/auth/signin')
         .send({ email, password: '123456' }))
@@ -10,4 +10,31 @@ test('Deve receber token ao logar', () => {
         expect(res.status).toBe(200);
         expect(res.body).toHaveProperty('token');
     });
+});
+
+test('Não deve autenticar usuário com senha errada', () => {
+    const email = `${Date.now()}@mail.com`;
+    return app.services.user.save({ name: 'maria veridiane', email , password: '123456' })
+    .then(() => request(app).post('/auth/signin')
+        .send({ email, password: '654321' }))
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
+
+test('Não deve autenticar com usuário e senha errados', () => {
+    return request(app).post('/auth/signin')
+        .send({ email: 'naoexisto@mail.com', password: '654321' })
+    .then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
+
+test('Não deve acessar uma rota protegida sem token', () => {
+    return request(app).get('/users')
+    .then((res) => {
+        expect(res.status).toBe(401);
+    })
 });
