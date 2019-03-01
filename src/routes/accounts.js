7e897b1b41e12ca417282b05c7f5e54514828e85
m3n0r <wesleysd1989@gmail.com>
2019-03-01 03:57:25 -0300

    2 test
    Não deve alterar uma conta de outro usuário
    
    Não deve remover uma conta de outro usuário

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
+const RecursoIndevidoError = require('../errors/RecursoIndevidoError');
+
 module.exports = (app) => {
     const router = express.Router();
+
+    router.param('id', (req, res, next) => {
+        app.services.account.find({ id: req.params.id })
+        .then((acc) => {
+            if(acc.user_id !== req.user.id) throw new RecursoIndevidoError();
+            else next();
+        }).catch(err => next(err));
+    });
 
     router.post('/', (req, res, next) => {
         app.services.account.save({ ... req.body, user_id: req.user.id })
@@ -12,23 +22,19 @@ module.exports = (app) => {
 
     router.get('/', (req, res, next) => {
         app.services.account.findAll(req.user.id)
-        .then((result) => res.status(200).json(result))
+        .then(result => res.status(200).json(result))
         .catch(err => next(err));
     });
 
     router.get('/:id', (req, res, next) => {
         app.services.account.find({id: req.params.id})
-        .then((result) => {
-            if(result.user_id !== req.user.id)
-                return res.status(403).json({ error: 'Este recurso não pertence ao usuário' });
-            return res.status(200).json(result)
-        })
+        .then(result => res.status(200).json(result))
         .catch(err => next(err));
     });
 
     router.put('/:id', (req, res, next) => {
         app.services.account.update(req.params.id, req.body)
-        .then((result) => res.status(200).json(result[0]))
+        .then(result => res.status(200).json(result[0]))
         .catch(err => next(err));
     });
 
--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -94,7 +94,17 @@ test('Deve alterar uma conta', () => {
     });
 });
 
-test.skip('Não deve alterar uma conta de outro usuário', () => { });
+test('Não deve alterar uma conta de outro usuário', () => {
+    return app.db('accounts')
+    .insert({ name: 'Acc User #2', user_id: user2.id }, ['id'])
+    .then(acc => request(app).put(`${MAIN_ROUTE}/${acc[0].id}`)
+        .send({ name: 'Acc Updated' })
+        .set('authorization', `bearer ${user.token}`))
+    .then((res) => {
+        expect(res.status).toBe(403);
+        expect(res.body.error).toBe('Este recurso não pertence ao usuário');
+    });
+});
 
 test('Deve remover uma conta', () => {
     return app.db('accounts')
@@ -105,4 +115,13 @@ test('Deve remover uma conta', () => {
     });
 });
 
-test.skip('Não deve remover uma conta de outro usuário', () => { });
+test('Não deve remover uma conta de outro usuário', () => {
+    return app.db('accounts')
+    .insert({ name: 'Acc User #2', user_id: user2.id }, ['id'])
+    .then(acc => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`)
+        .set('authorization', `bearer ${user.token}`))
+    .then((res) => {
+        expect(res.status).toBe(403);
+        expect(res.body.error).toBe('Este recurso não pertence ao usuário');
+    });
+});
