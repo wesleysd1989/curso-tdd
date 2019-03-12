cfeef76ec3dc754e85a4707c7eff462555156e49
m3n0r <wesleysd1989@gmail.com>
2019-03-12 05:39:18 -0300

    Update account.test.js

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -7,7 +7,7 @@ const MAIN_ROUTE = '/v1/accounts';
 let user;
 let user2;
 
-beforeEach(async () => {
+beforeAll(async () => {
     const res = await app.services.user.save({name: 'User Account', email: `${Date.now()}@mail.com`, password: '123456'});
     user = { ... res[0] };
     user.token = jwt.encode(user, 'Segredo!');
@@ -46,7 +46,9 @@ test('Não deve inserir uma conta de nome duplicado, para o mesmo usuário', () 
         })
 });
 
-test('Deve listar apenas as contas do usuário', () => { 
+test('Deve listar apenas as contas do usuário', async () => {
+    await app.db('transactions').del();
+    await app.db('accounts').del();
     return app.db('accounts').insert([
         { name: 'acc user #1', user_id: user.id },
         { name: 'acc user #2', user_id: user2.id },
