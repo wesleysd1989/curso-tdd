f2ca502b34e7380a44ed77a85f2352ba80a05265
m3n0r <wesleysd1989@gmail.com>
2019-03-13 04:39:58 -0300

    test Ao alterar uma tranferência valida ...

--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -111,3 +111,46 @@ test('Deve retornar uma transferência por Id', () => {
          expect(res.body.description).toBe('Transfer #1');
     });
 });
+
+describe('Ao alterar uma tranferência valida ...', () => {
+    let transferId;
+    let income;
+    let outcome
+
+    test('Deve retornar o status 200 e os dados da transferência', () => {
+        return request(app).put(`${MAIN_ROUTE}/100000`)
+        .set('authorization', `bearer ${TOKEN}`)
+        .send({ description: 'Transfer Updated', user_id: 100000, acc_ori_id: 100000, acc_dest_id: 100001, ammount: 500, date: new Date() })
+        .then(async (res) => {
+            expect(res.status).toBe(200);
+            expect(res.body.description).toBe('Transfer Updated');
+            expect(res.body.ammount).toBe('500.00');
+            transferId = res.body.id;
+        });
+    });
+
+    test('As transações equivalentes devem ter sido geradas', async () => {
+        const transactions = await app.db('transactions').where({ transfer_id: transferId }).orderBy('ammount');
+        expect(transactions).toHaveLength(2);
+        [outcome, income] = transactions;
+    });
+
+    test('A transação de saída deve ser negativa', () => {
+        expect(outcome.description).toBe('Transfer to acc #100001');
+        expect(outcome.ammount).toBe('-500.00');
+        expect(outcome.acc_id).toBe(100000);
+        expect(outcome.type).toBe('O');
+    });
+
+    test('A transação de entrada deve ser positiva', () => {
+        expect(income.description).toBe('Transfer from acc #100000');
+        expect(income.ammount).toBe('500.00');
+        expect(income.acc_id).toBe(100001);
+        expect(income.type).toBe('I');
+    });
+
+    test('ambas devem referenciar a transferência que as originou', () => {
+        expect(income.transfer_id).toBe(transferId);
+        expect(outcome.transfer_id).toBe(transferId);
+    });
+});
