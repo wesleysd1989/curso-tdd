33c7e31c79e84da01dda07d3f250396134463fb1
m3n0r <wesleysd1989@gmail.com>
2019-03-13 03:07:37 -0300

    test Deve inserir uma transferência com sucesso

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -9,5 +9,18 @@ module.exports = (app) => {
         .select();
     };
 
-    return { find }
+    const save = async (transfer) => {
+        const result = await app.db('transfers').insert(transfer, '*');
+        const transferId = result[0].id;
+
+        const transactions = [
+            { description: `Transfer to acc #${transfer.acc_dest_id}`, date: transfer.date, ammount: transfer.ammount * -1, type: 'O', acc_id: transfer.acc_ori_id, transfer_id: transferId},
+            { description: `Transfer from acc #${transfer.acc_ori_id}`, date: transfer.date, ammount: transfer.ammount, type: 'I', acc_id: transfer.acc_dest_id, transfer_id: transferId},
+        ]
+        await app.db('transactions').insert(transactions);
+        return result;
+        
+    };
+
+    return { find, save }
 };
--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -20,3 +20,21 @@ test('Deve listar apenas as transferências do usuários', () => {
     });
 });
 
+test('Deve inserir uma transferência com sucesso', () => {
+    return request(app).post(MAIN_ROUTE)
+        .set('authorization', `bearer ${TOKEN}`)
+        .send({ description: 'Regular Transfer', user_id: 100000, acc_ori_id: 100000, acc_dest_id: 100001, ammount: 100, date: new Date() })
+        .then(async (res) => {
+            expect(res.status).toBe(201);
+            expect(res.body.description).toBe('Regular Transfer');
+
+            const transactions = await app.db('transactions').where({ transfer_id: res.body.id });
+            expect(transactions).toHaveLength(2);
+            expect(transactions[0].description).toBe('Transfer to acc #100001');
+            expect(transactions[1].description).toBe('Transfer from acc #100000');
+            expect(transactions[0].ammount).toBe('-100.00');
+            expect(transactions[1].ammount).toBe('100.00');
+            expect(transactions[0].acc_id).toBe(100000);
+            expect(transactions[1].acc_id).toBe(100001);
+    });
+});
